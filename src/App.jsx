7fce0cd12f02fd5c690af806aef3be4ad4141e24
fabrciovvieira <fabriciovvieira@gmail.com>
2024-03-25 17:39:19 -0300

    Ajustes de responsividade

--- src/App.jsx
+++ src/App.jsx
@@ -11,15 +11,10 @@ function App() {
 
   return (
     <>
-      <div className="general-warning-responsive">
-      <h1 className='h1-responsive'>Infelizmente esse site não tem ajustes de tela com seu dispositivo</h1>
-      <p className="p-responsive">Tente usar um dispositivo com uma tela maior</p>
-      <p className="p-responsive-ideal">Ideal para uso em laptops</p>
         <NavBar />
         <div className="container">
           <Outlet />
         </div>
-      </div>
     </>
   )
 }
--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,5 +1,5 @@
 // REACT
-import React from 'react'
+import React, { useState} from 'react'
 import { Link } from 'react-router-dom'
 import { BiCameraMovie } from "react-icons/bi";
 
@@ -10,15 +10,46 @@ import '../navBar/NavBar.css'
 
 
 const NavBar = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setIsOpen(!isOpen);
+    };
+
   return (
     <div className='div-container'>
         <nav className="navbar-containter">
             <div className="nav-content">
                 <div className="logo-container">
-                    <BiCameraMovie size={"3rem"}/>
+                    <BiCameraMovie className='biCameraMovie'/>
                     <h2 className='logo'>Watch Out</h2>
                 </div>
-                <ul>
+                <div className="hamburger-menu">
+                    <div className={`menu-btn ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+                        <div className="btn-line" />
+                        <div className="btn-line" />
+                        <div className="btn-line" />
+                    </div>
+                    <nav className={`menu ${isOpen ? 'open' : ''}`}>
+                        <ul className='list-navbar-mobile'>
+                            <li>
+                                <BiCameraMovie className='biCameraMovie-mobile'/>
+                            </li>
+                            <li>
+                                <Link className='menu-item-mobile' to="/api_TMDB_Com_React" onClick={toggleMenu}>
+                                    Home
+                                </Link>
+                            </li>
+                            <li>
+                                <Link className='menu-item-mobile' to="/api_TMDB_Com_React/sugestao" onClick={toggleMenu}>
+                                    Minhas redes sociais
+                                </Link>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+                <ul className='list-navBar'>
                     <li>
                         <Link className='menu-item' to="/api_TMDB_Com_React">
                             Home
